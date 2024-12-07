1844b97e470449e1f5babf53ef72373948c134a4
Timofey_Babisashvili <timofey.babisashvili@gmail.com>
2024-12-07 17:51:46 +0200

    origin: '*' in main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4000', // Адрес вашего фронтенда
+    // origin: 'http://localhost:4000', // Адрес вашего фронтенда
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
